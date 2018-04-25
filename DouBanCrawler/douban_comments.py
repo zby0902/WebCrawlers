9a22cfb95dfa4255cb5b2d940bedc3c509045bc5
zby0902 <zbyroger@gmail.com>
2018-04-25 17:24:15 +1000

    finish first command line crawler with douban as example

---
+++ DouBanCrawler/douban_comments.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+This is a simplest script that will take all html content of a url and
+then save them into a text file.
+It has one command line parameter which is --url or -u.
+If no command line parameter is given, it uses its default url wich is:
+'http://www.baidu.com'
+"""
+import requests
+import argparse	
+ap = argparse.ArgumentParser()
+ap.add_argument('-u','--url',help="url path of the website")
+args = vars(ap.parse_args())
+def getHtmlText(url='http://www.baidu.com'):
+	try:
+		r = requests.get(url, timeout=20)
+		r.raise_for_status()
+		r.encoding = r.apparent_encoding
+		with open('./result.txt','w') as f:
+			f.write(str(r.text))
+	except:
+		return "Exception encounter!"
+if __name__ == '__main__':
+	if args['url']:
+		getHtmlText(url=args['url'])
+	else:
+		getHtmlText()
